01f58e1f6c98060154fbd68f3cb5900b6bd9274a
lanita <lanita@lanitas-MacBook-Pro.local>
2025-12-12 20:09:16 -0500

    Add async database connection

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
+import os
+
+DATABASE_URL = os.environ["DATABASE_URL"].replace(
+    "postgresql://", "postgresql+asyncpg://"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 app = FastAPI(title="ProbLabs API")
 
